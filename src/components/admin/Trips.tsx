cdc4ba2687061546c78ee086200aabdfb86dd9b6
amenolabode <olabodeamen4@gmail.com>
2023-01-03 16:08:59 +0000

    Admin - Trip Overview Updates: Create, Edit Del UI

--- src/components/admin/Trips.tsx
+++ src/components/admin/Trips.tsx
@@ -6,137 +6,140 @@ import { Modal } from "antd";
 import CreateTripFormComponent from "./components/create-trip-form";
 import { useAppDispatch, useAppSelector } from "../../state/hooks";
 import {
-	getAllAvailableTripAction,
-	getAllTripAction,
+  getAllAvailableTripAction,
+  getAllTripAction,
 } from "../../state/action/trip.action";
 import { Spinner } from "react-bootstrap";
+import { FaCheckCircle } from "react-icons/fa";
 // import {useAppSelector} from '../../../'
 
 const MiddleSection: React.FC = () => {
-	// PAGINATION
-	const dispatch = useAppDispatch();
-	const { trips, loading, error } = useAppSelector(
-		(state: any) => state.allTrip
-	);
+  // PAGINATION
+  const dispatch = useAppDispatch();
+  const { trips, loading, error } = useAppSelector(
+    (state: any) => state.allTrip
+  );
 
-	const [activeTripsView, setIsActive] = useState("overview");
-	const handleTripViewToggle = (value: string) => {
-		setIsActive(value);
-	};
+  const [activeTripsView, setIsActive] = useState("overview");
+  const handleTripViewToggle = (value: string) => {
+    setIsActive(value);
+  };
 
-	const [flip, setFlip] = useState("");
-	const [createVisible, setCreateModalVisible] = useState<boolean>(false);
-	const handleOpenCreateModal = () => {
-		setFlip("create");
-		setCreateModalVisible(true);
-		// setModalData(data);
-	};
-	const handleOk = () => {
-		setCreateModalVisible(false);
-	};
+  const [flip, setFlip] = useState("");
+  const [createVisible, setCreateModalVisible] = useState<boolean>(false);
+  const handleOpenCreateModal = () => {
+    setFlip("create");
+    setCreateModalVisible(true);
+    // setModalData(data);
+  };
+  const handleOk = () => {
+    setCreateModalVisible(false);
+  };
 
-	const handleCancel = () => {
-		if (flip === "create") {
-			setCreateModalVisible(false);
-			setFlip("");
-		}
-	};
+  const handleCancel = () => {
+    if (flip === "create") {
+      setCreateModalVisible(false);
+      setFlip("");
+    }
+  };
 
-	//ALL COLLECTED DATA FROM FORM FIELDS
-	const [startCity, setStartCity] = useState("");
-	const [startBusStop, setStartBusStop] = useState("");
-	const [destinationCity, setDestinationCity] = useState("");
-	const [destinationBusStop, setDestinationBusStop] = useState("");
-	const [driver, setDriver] = useState("");
-	const [vehicle, setVehicle] = useState("");
-	const [date, setDate] = useState("");
-	const [time, setTime] = useState("");
+  //ALL COLLECTED DATA FROM FORM FIELDS
+  const [startCity, setStartCity] = useState("");
+  const [startBusStop, setStartBusStop] = useState("");
+  const [destinationCity, setDestinationCity] = useState("");
+  const [destinationBusStop, setDestinationBusStop] = useState("");
+  const [driver, setDriver] = useState("");
+  const [vehicle, setVehicle] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
 
-	const handleDataFromChild = (
-		startCityDisplayText: any,
-		startBusStopDisplayText: any,
-		destinationCityDisplayText: any,
-		destinationBuStopDisplayText: any,
-		driverDisplayText: any,
-		VehicleDisplayText: any,
-		year: any,
-		month: any,
-		day: any,
-		time: any
-	) => {
-		setStartCity(startCityDisplayText);
-		setStartBusStop(startBusStopDisplayText);
-		setDestinationCity(destinationCityDisplayText);
-		setDestinationBusStop(destinationBuStopDisplayText);
-		setDriver(driverDisplayText);
-		setVehicle(VehicleDisplayText);
-		setDate(year + "-" + month + "-" + day);
-		setTime(time);
-	};
+  const handleDataFromChild = (
+    startCityDisplayText: any,
+    startBusStopDisplayText: any,
+    destinationCityDisplayText: any,
+    destinationBuStopDisplayText: any,
+    driverDisplayText: any,
+    VehicleDisplayText: any,
+    year: any,
+    month: any,
+    day: any,
+    time: any
+  ) => {
+    setStartCity(startCityDisplayText);
+    setStartBusStop(startBusStopDisplayText);
+    setDestinationCity(destinationCityDisplayText);
+    setDestinationBusStop(destinationBuStopDisplayText);
+    setDriver(driverDisplayText);
+    setVehicle(VehicleDisplayText);
+    setDate(year + "-" + month + "-" + day);
+    setTime(time);
+  };
 
-	const validation =
-		startBusStop !== "Select Start Bus Stop" &&
-		startCity !== "Select Start City" &&
-		destinationBusStop !== "Select Destination Bus Stop" &&
-		destinationCity !== "Select Destination City" &&
-		driver !== "Select Driver" &&
-		date !== "--" &&
-		vehicle !== "Select Vehicle" &&
-		startCity !== destinationCity &&
-		time !== "";
+  const validation =
+    startBusStop !== "Select Start Bus Stop" &&
+    startCity !== "Select Start City" &&
+    destinationBusStop !== "Select Destination Bus Stop" &&
+    destinationCity !== "Select Destination City" &&
+    driver !== "Select Driver" &&
+    date !== "--" &&
+    vehicle !== "Select Vehicle" &&
+    startCity !== destinationCity &&
+    time !== "";
 
-	useEffect(() => {
-		dispatch(getAllTripAction());
-	}, [dispatch]);
-	return (
-		<div className="bg-white h-full col-start-2 col-end-6 ">
-			<div className="bg-white h-full px-6">
-				{/* GROUP BUTTON - NAVIGATION */}
-				<div className="border-b w-full mt-8 py-4 mb-2">
-					<div className="inline-flex rounded-md" role="group">
-						<button
-							onClick={() => handleTripViewToggle("overview")}
-							type="button"
-							className={`inline-flex items-center py-2 px-6 text-base font-medium  ${
-								activeTripsView === "overview"
-									? "bg-primary-100 font-semibold text-black"
-									: "text-gray-400 font-normal bg-gray-50"
-							}`}>
-							Trips Overview
-						</button>
+  useEffect(() => {
+    dispatch(getAllTripAction());
+  }, [dispatch]);
+  return (
+    <div className="bg-white h-full col-start-2 col-end-6 ">
+      <div className="bg-white h-full px-6">
+        {/* GROUP BUTTON - NAVIGATION */}
+        <div className="border-b w-full mt-8 py-4 mb-2">
+          <div className="inline-flex rounded-md" role="group">
+            <button
+              onClick={() => handleTripViewToggle("overview")}
+              type="button"
+              className={`inline-flex items-center py-2 px-6 text-base font-medium  ${
+                activeTripsView === "overview"
+                  ? "bg-primary-100 font-semibold text-black"
+                  : "text-gray-400 font-normal bg-gray-50"
+              }`}
+            >
+              Trips Overview
+            </button>
 
-						<button
-							onClick={() => handleTripViewToggle("management")}
-							type="button"
-							className={`inline-flex items-center py-2 px-8 text-base font-medium  ${
-								activeTripsView === "management"
-									? "bg-primary-100 font-semibold text-black"
-									: "text-gray-400 font-normal bg-gray-50"
-							}`}>
-							Bus Stops Management
-						</button>
-					</div>
-				</div>
+            <button
+              onClick={() => handleTripViewToggle("management")}
+              type="button"
+              className={`inline-flex items-center py-2 px-8 text-base font-medium  ${
+                activeTripsView === "management"
+                  ? "bg-primary-100 font-semibold text-black"
+                  : "text-gray-400 font-normal bg-gray-50"
+              }`}
+            >
+              Bus Stops Management
+            </button>
+          </div>
+        </div>
 
-				{/* BUSSTOPS HEADER */}
-				<div className="border-b h-14 w-full my-2">
-					<div className="flex justify-between">
-						<h2 className="text-lg mt-2 font-medium">Busstops</h2>{" "}
-						{loading && <Spinner />}
-						{activeTripsView === "overview" ? (
-							<Button
-								title="+ Create new trip"
-								type="submit"
-								className="px-4 py-2 text-xs rounded-md bg-primary-100"
-								onClick={() => {
-									handleOpenCreateModal();
-								}}
-							/>
-						) : (
-							""
-						)}
-					</div>
-				</div>
+        {/* BUSSTOPS HEADER */}
+        <div className="border-b h-14 w-full my-2">
+          <div className="flex justify-between">
+            <h2 className="text-lg mt-2 font-medium">Busstops</h2>{" "}
+            {loading && <Spinner />}
+            {activeTripsView === "overview" ? (
+              <Button
+                title="+ Create new trip"
+                type="submit"
+                className="px-4 py-2 text-xs rounded-md bg-primary-100"
+                onClick={() => {
+                  handleOpenCreateModal();
+                }}
+              />
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
 
         {/* CREATE TRIP MODAL */}
         {flip === "create"
@@ -188,7 +191,7 @@ const MiddleSection: React.FC = () => {
                       stroke-width="5"
                     />
                   </svg>
-                  Create Trip
+                  Proceed to review
                 </button>
               </Modal>
             )
@@ -214,7 +217,7 @@ const MiddleSection: React.FC = () => {
                     </div>
                     <div className="text-lg">{startCity}</div>
                   </div>
-                  
+
                   <div>
                     <div className="text-sm text-gray-400 font-normal mb-1">
                       Destination
@@ -263,9 +266,8 @@ const MiddleSection: React.FC = () => {
                   type="submit"
                   className="w-full px-4 py-3 text-xs rounded-md bg-primary-100"
                   onClick={() => {
-                    handleOk()
+                    setFlip("success");
                     //aAPI CALL HERE FOR CREATING A TRIP
-                   
                   }}
                 />
                 <Button
@@ -278,16 +280,47 @@ const MiddleSection: React.FC = () => {
                 />
               </Modal>
             )
+          : flip === "success"
+          ? createVisible && (
+              <Modal
+                onOk={handleOk}
+                onCancel={handleCancel}
+                open={createVisible}
+                centered={true}
+                footer={false}
+                closable={true}
+                width={240}
+              >
+                <div className="w-full place-items-center text-center">
+                  <FaCheckCircle
+                    size={32}
+                    className="text-[#00FF6A] w-full mt-8"
+                  />
+                  <div className="boder-b mt-4 text-base font-medium">
+                    Trip succesfully created
+                  </div>
+                </div>
+
+                <Button
+                  title="Close"
+                  type="submit"
+                  className="w-full py-2 mt-8 mb-4 text-xs rounded-md bg-[#00FF6A] text-black"
+                  onClick={() => {
+                    setCreateModalVisible(false);
+                  }}
+                />
+              </Modal>
+            )
           : null}
 
-				{activeTripsView === "overview" ? (
-					<TripsOverview />
-				) : activeTripsView === "management" ? (
-					<BusStopManagement />
-				) : null}
-			</div>
-		</div>
-	);
+        {activeTripsView === "overview" ? (
+          <TripsOverview />
+        ) : activeTripsView === "management" ? (
+          <BusStopManagement />
+        ) : null}
+      </div>
+    </div>
+  );
 };
 
 export default MiddleSection;
