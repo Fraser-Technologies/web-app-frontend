5c66f4d00dcabd4832d9101fb9a8d189f8cca172
LakeCombs <adesolaolamilekan2@gmail.com>
2023-02-28 07:19:01 +0100

    route helper

--- src/utils/route_helper.ts
+++ src/utils/route_helper.ts
@@ -3,15 +3,24 @@ import { APPS } from "./route_constant";
 const getSubdomain = (location: string) => {
 	const locationParts = location.split(".");
 	console.log("the location parts is ", locationParts);
-	let sliceTil = -2;
+	// let sliceTil = -2;
 
 	const isLocalhost =
 		locationParts.slice(-1)[0] === process.env.production
 			? "ridefraser"
 			: "localhost:3000";
 
-	if (isLocalhost) sliceTil = -1;
-	return locationParts.slice(0, sliceTil).join(".");
+	console.log("the is localhost is ", isLocalhost);
+
+	// if (isLocalhost) sliceTil = -1;
+	// return locationParts.slice(0, sliceTil).join(".");
+	// return locationParts[0];
+
+	if (isLocalhost) {
+		return;
+	}
+
+	return locationParts[0];
 };
 
 export const getApp = () => {
