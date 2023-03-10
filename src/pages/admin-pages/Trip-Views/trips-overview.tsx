12d12bc48f02bfe992c28d8e36aaa461cc7bfd26
amenolabode <olabodeamen4@gmail.com>
2023-03-10 08:13:06 +0000

    ux updates on manifest

--- src/pages/admin-pages/Trip-Views/trips-overview.tsx
+++ src/pages/admin-pages/Trip-Views/trips-overview.tsx
@@ -109,6 +109,9 @@ const TripsOverview: React.FC = () => {
     setModalVisible(true);
   };
 
+  const [loading, setLoading] = useState(false);
+  const [loadingIndex, setLoadingIndex] = useState<number>(-1);
+
   const [visible, setStateModalVisible] = useState<boolean>(false);
   const handleOpenDeleteModal = (data: Trip_interface) => {
     setFlip(TripOption.DELETE);
@@ -177,6 +180,7 @@ const TripsOverview: React.FC = () => {
 
   useEffect(() => {
     dispatch(getAllTripAction());
+    setLoading(false);
   }, [dispatch, onBoardedTrip, userInfo, unBoardedTrip]);
 
   useEffect(() => {
@@ -530,9 +534,61 @@ const TripsOverview: React.FC = () => {
       {flip === TripOption.MANIFEST && modalVisible && (
         <Modal
           title={
-            <div className="text-lg font-medium boder-b">
-              {`${modalData?.travel_destination?.from?.city?.city} to
-							${modalData?.travel_destination?.to?.city?.city} Trip`}
+            <div className="">
+              <p className="mb-4 text-lg font-medium">Passenger Manifest</p>
+              <p className="text-base font-normal">{`${modalData?.travel_destination?.from?.city?.city} to ${modalData?.travel_destination?.to?.city?.city} Trip`}</p>
+
+              <div className="flex w-full bg-black rounded-md px-2 pt-1 pb-3 font-normal text-[14px] text-white my-2">
+                <div className="w-full flex mt-2 mr-2 rounded-md ">
+                  <div className="ml-2">
+                    <p className="mb-1 text-gray-500">Passengers</p>
+                    <p className="text-lg">{modalData?.passengers.length} </p>
+                  </div>
+                </div>
+                <div className="w-full flex mt-2 mr-2 rounded-md ">
+                  <div className="ml-2">
+                    <p className="mb-1 text-gray-500">Onboard</p>
+                    <p className=" text-lg">
+                      {
+                        modalData?.passengers?.filter(
+                          (item: any) => item.isOnboard
+                        ).length
+                      }{" "}
+                    </p>
+                  </div>
+                </div>
+                <div className="w-full flex mt-2 mr-2 rounded-md ">
+                  <div className="ml-2">
+                    <p className="mb-1 text-gray-500">Not Onboard</p>
+                    <p className=" text-lg">
+                      {
+                        modalData?.passengers?.filter(
+                          (item: any) => !item.isOnboard
+                        ).length
+                      }
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <table className="w-full mt-4 text-base font-normal text-left text-white table-auto">
+                <thead className="bg-black w-full text-white">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-2 py-2 pl-4 font-normal rounded-l-md"
+                    >
+                      Name
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-2 py-2 font-normal text-center rounded-r-md"
+                    >
+                      Action
+                    </th>
+                  </tr>
+                </thead>
+              </table>
             </div>
           }
           onOk={handleOk}
@@ -542,45 +598,23 @@ const TripsOverview: React.FC = () => {
           footer={false}
           closable={true}
         >
-          <div>
-            <div className="mt-6 mb-4 text-base font-medium">
-              Passenger Manifest
-            </div>
-            <div className="my-1 text-gray-400">
-              {modalData?.passengers.length} Passengers,{" "}
-              {
-                modalData?.passengers?.filter((item: any) => item.isOnboard)
-                  .length
-              }
-              Onboard,{" "}
-              {
-                modalData?.passengers?.filter((item: any) => !item.isOnboard)
-                  .length
-              }{" "}
-              Not Onboard
-            </div>
+          <div className="h-[60vh] overflow-y-scroll">
             <table className="w-full mt-2 text-base font-normal text-left text-white table-auto">
-              <thead className="bg-black">
-                <tr>
-                  <th
-                    scope="col"
-                    className="px-2 py-2 pl-4 font-normal rounded-mdlg"
-                  >
-                    Name
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-2 py-2 font-normal text-center rounded-mdlg"
-                  >
-                    Action
-                  </th>
-                </tr>
-              </thead>
-
               {/* //TABLE ROWS */}
               <tbody className="mt-4">
-                {modalData?.passengers?.map(
-                  (passenger: Passenger_interface) => {
+                {modalData?.passengers
+                  ?.slice()
+                  .sort((a: { isOnboard: any }, b: { isOnboard: any }) => {
+                    if (a?.isOnboard && !b?.isOnboard) {
+                      return 1;
+                    }
+                    if (!a?.isOnboard && b?.isOnboard) {
+                      return -1;
+                    }
+                    return 0;
+                  })
+                  .map((passenger: Passenger_interface, index: number) => {
+                    const isLoading = loadingIndex === index;
                     return (
                       <tr
                         key={passenger._id}
@@ -625,19 +659,43 @@ const TripsOverview: React.FC = () => {
                                 <div
                                   className="flex flex-row items-center"
                                   onClick={() => {
+                                    setLoadingIndex(index);
                                     dispatch(
                                       verifyPassengerOnboardAction(
                                         modalData?._id,
                                         passenger?._id
                                       )
-                                    );
+                                    ).finally(() => {
+                                      setLoadingIndex(-1);
+                                    });
                                   }}
                                 >
-                                  <FaCheck
-                                    className="mr-2"
-                                    onClick={() => {}}
-                                  />
-                                  <span>Onboard</span>
+                                  {isLoading ? (
+                                    <svg
+                                      className={`${
+                                        isLoading ? "animate-spin" : "hidden"
+                                      } inline mr-2 w-4 h-4 text-gray-200 dark:text-gray-600 fill-blue-600`}
+                                      viewBox="0 0 100 101"
+                                      fill="none"
+                                      xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                      <path
+                                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                        fill="white"
+                                        stroke="white"
+                                        stroke-width="5"
+                                      />
+                                      <path
+                                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                        fill="green"
+                                        stroke="green"
+                                        stroke-width="5"
+                                      />
+                                    </svg>
+                                  ) : (
+                                    <FaCheck className="mr-2" />
+                                  )}
+                                  <span>Onboard </span>
                                 </div>
                               )}
                             </div>
@@ -655,10 +713,7 @@ const TripsOverview: React.FC = () => {
                         </td>
                       </tr>
                     );
-                  }
-                )}
-
-                {/* )} */}
+                  })}
               </tbody>
             </table>
           </div>
