58b638c5f0482307e001829498998ec07677545c
LakeCombs <adesolaolamilekan2@gmail.com>
2023-03-22 20:31:58 +0000

    he worked on loigin

--- src/pages/Waitlist.tsx
+++ src/pages/Waitlist.tsx
@@ -8,228 +8,224 @@ import { Input, Modal } from "antd";
 import { useNavigate } from "react-router-dom";
 import { _paths_ } from "../utils/routes";
 
-
 const Waitlist = () => {
-  const [modal, openModal] = useState(false);
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
+	const [modal, openModal] = useState(false);
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+	const [email, setEmail] = useState("");
+	const [loading, setLoading] = useState(false);
+	const navigate = useNavigate();
 
-  const emailRegex =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	const emailRegex =
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  const loginValid =
-    firstName !== "" &&
-    lastName !== "" &&
-    email !== "" &&
-    email.match(emailRegex);
+	const loginValid =
+		firstName !== "" &&
+		lastName !== "" &&
+		email !== "" &&
+		email.match(emailRegex);
 
-  //   const [modalVisible, setModalVisible] = useState<boolean>(false);
-  const handleOk = () => {
-    openModal(false);
-  };
+	//   const [modalVisible, setModalVisible] = useState<boolean>(false);
+	const handleOk = () => {
+		openModal(false);
+	};
 
-  const handleCancel = () => {
-    openModal(false);
-  };
+	const handleCancel = () => {
+		openModal(false);
+	};
 
-  const usersCollectionRef = collection(db, "users");
+	const usersCollectionRef = collection(db, "users");
 
-  const handleSubmit = async (e: { preventDefault: () => void }) => {
-    e.preventDefault();
-    if (firstName.length !== 0 && lastName.length !== 0 && email.length !== 0) {
-      //   alert("firebase runs");
-      setLoading(true);
-      await addDoc(usersCollectionRef, {
-        firstname: firstName,
-        lastname: lastName,
-        email: email,
-        CreatedAt: serverTimestamp(),
-      })
-        .then(() => {
-          setLoading(false);
-          alert(
-            "Thank you for joining our waitlist. We will keep you updated once we launch. Welcome to the good side."
-          );
-        })
-        .catch((error: any) => {
-          alert(error.message);
-        });
+	const handleSubmit = async (e: { preventDefault: () => void }) => {
+		e.preventDefault();
+		if (firstName.length !== 0 && lastName.length !== 0 && email.length !== 0) {
+			//   alert("firebase runs");
+			setLoading(true);
+			await addDoc(usersCollectionRef, {
+				firstname: firstName,
+				lastname: lastName,
+				email: email,
+				CreatedAt: serverTimestamp(),
+			})
+				.then(() => {
+					setLoading(false);
+					alert(
+						"Thank you for joining our waitlist. We will keep you updated once we launch. Welcome to the good side."
+					);
+				})
+				.catch((error: any) => {
+					alert(error.message);
+				});
 
-      setEmail("");
-      setFirstName("");
-      setLastName("");
-      openModal(false);
-    } else alert("Empty Fields");
-  };
+			setEmail("");
+			setFirstName("");
+			setLastName("");
+			openModal(false);
+		} else alert("Empty Fields");
+	};
 
-  return (
-    // <Layout title="Waitlist">
-    <div className="w-full bg-black text-white">
-      <div className="w-full h-[100vh] overflow-y-scroll md:overflow-hidden">
-        <div className="text-center">
-          <h1 className="mt-8 text-[32px]">Fraser</h1>
+	return (
+		// <Layout title="Waitlist">
+		<div className="w-full bg-black text-white">
+			<div className="w-full h-[100vh] overflow-y-scroll md:overflow-hidden">
+				<div className="text-center">
+					<h1 className="mt-8 text-[32px]">Fraser</h1>
 
-          <div className="px-4 mt-12 md:mt-40 lg:mt-28 mb-6 text-[64px] md:text-[80px] lg:text-[120px] font-medium md:font-semibold leading-none md:leading-12 md:tracking-tight">
-            <div className="text-primary-100">Move freely</div>
-            <div className="w-full">between cities</div>
-          </div>
-          <p className="px-4 w-full md:w-4/6 lg:w-1/3 md:mx-auto mb-8 md:mb-16 lg:mb-8 text-gray-300 md:text-[22px]">
-            Travel comfortably, conveniently and affordably between cities with Fraser.
-          </p>
-          
+					<div className="px-4 mt-12 md:mt-40 lg:mt-28 mb-6 text-[64px] md:text-[80px] lg:text-[120px] font-medium md:font-semibold leading-none md:leading-12 md:tracking-tight">
+						<div className="text-primary-100">Move freely</div>
+						<div className="w-full">between cities</div>
+					</div>
+					<p className="px-4 w-full md:w-4/6 lg:w-1/3 md:mx-auto mb-8 md:mb-16 lg:mb-8 text-gray-300 md:text-[22px]">
+						Travel comfortably, conveniently and affordably between cities with
+						Fraser.
+					</p>
 
-          <div className="mx-8 md:w-1/2 lg:w-1/4 md:mx-auto">
-            <button
-              className="py-6 lg:py-8 lg:px-16 w-full rounded-xl text-[18px] bg-[#00ff6a] hover:bg-[#0be466] text-black mb-4 lg:mb-6"
-              onClick={() => {
-                openModal(true);
-              }}
-            >
-              Join Waitlist
-            </button>
-            <button
-              className="py-6 md:py-8 md:px-16 w-full rounded-xl text-[18px] border border-[#ffffff] hover:border-[#e3e3e3] hover:text-[#e3e3e3] text-white"
-              onClick={() => {
-                navigate(_paths_.BOOKRIDE);
-              }}
-            >
-              Get Early Access 👀
-            </button>
-          </div>
+					<div className="mx-8 md:w-1/2 lg:w-1/4 md:mx-auto">
+						<button
+							className="py-6 lg:py-8 lg:px-16 w-full rounded-xl text-[18px] bg-[#00ff6a] hover:bg-[#0be466] text-black mb-4 lg:mb-6"
+							onClick={() => {
+								openModal(true);
+							}}>
+							Join Waitlist
+						</button>
+						<button
+							className="py-6 md:py-8 md:px-16 w-full rounded-xl text-[18px] border border-[#ffffff] hover:border-[#e3e3e3] hover:text-[#e3e3e3] text-white"
+							onClick={() => {
+								// navigate(_paths_.BOOKRIDE);
+								navigate(_paths_.SIGNUP);
+							}}>
+							Get Early Access 👀
+						</button>
+					</div>
 
-          <div className="h-[30vh] md:h-[50vh] mt-0 md:mt-12 lg:mt-0 lg:h-full overflow-hidden">
-          <div className="image-container ">
-            <div className="image-bg-left">
-              <div className="image-bg">
-                <img className="homepage" src="/Homepage.png" alt="" />
-              </div>
-            </div>
-          </div>
-          </div>
-        </div>
-      </div>
+					<div className="h-[30vh] md:h-[50vh] mt-0 md:mt-12 lg:mt-0 lg:h-full overflow-hidden">
+						<div className="image-container ">
+							<div className="image-bg-left">
+								<div className="image-bg">
+									<img className="homepage" src="/Homepage.png" alt="" />
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 
-      {/* Modal */}
+			{/* Modal */}
 
-      {modal && (
-        <Modal
-          title={
-            <p className="text-[16px] text-[#929292] mt-4 w-11/12">
-              Fraser goes the extra mile to ensure your convenience and safety
-              without breaking the bank.😊
-              <br />
-              <br />
-              P.S. Get free trips when you join our waitlist
-            </p>
-          }
-          onOk={handleOk}
-          onCancel={handleCancel}
-          open={modal}
-          centered={true}
-          footer={false}
-          closable={true}
-        >
-          <form onSubmit={handleSubmit}>
-            <div className="form-field mb-6 mt-8">
-              <label className="form-label" htmlFor="name">
-                First name
-              </label>
+			{modal && (
+				<Modal
+					title={
+						<p className="text-[16px] text-[#929292] mt-4 w-11/12">
+							Fraser goes the extra mile to ensure your convenience and safety
+							without breaking the bank.😊
+							<br />
+							<br />
+							P.S. Get free trips when you join our waitlist
+						</p>
+					}
+					onOk={handleOk}
+					onCancel={handleCancel}
+					open={modal}
+					centered={true}
+					footer={false}
+					closable={true}>
+					<form onSubmit={handleSubmit}>
+						<div className="form-field mb-6 mt-8">
+							<label className="form-label" htmlFor="name">
+								First name
+							</label>
 
-              <Input
-                className="w-full h-12 mt-2 hover:border-green-500 focus:border-green-500 active:border-green-600"
-                placeholder="First Name"
-                value={firstName}
-                type="text"
-                required={true}
-                onChange={(e) => setFirstName(e.target.value)}
-              />
-            </div>
+							<Input
+								className="w-full h-12 mt-2 hover:border-green-500 focus:border-green-500 active:border-green-600"
+								placeholder="First Name"
+								value={firstName}
+								type="text"
+								required={true}
+								onChange={(e) => setFirstName(e.target.value)}
+							/>
+						</div>
 
-            <div className="form-field mb-6">
-              <label className="form-label" htmlFor="name">
-                Last name
-              </label>
+						<div className="form-field mb-6">
+							<label className="form-label" htmlFor="name">
+								Last name
+							</label>
 
-              <Input
-                className="w-full h-12 mt-2 hover:border-green-500 focus:border-green-500 active:border-green-600"
-                placeholder="Last Name"
-                value={lastName}
-                type="text"
-                required={true}
-                onChange={(e) => setLastName(e.target.value)}
-              />
-            </div>
+							<Input
+								className="w-full h-12 mt-2 hover:border-green-500 focus:border-green-500 active:border-green-600"
+								placeholder="Last Name"
+								value={lastName}
+								type="text"
+								required={true}
+								onChange={(e) => setLastName(e.target.value)}
+							/>
+						</div>
 
-            <div className="form-field mb-6">
-              <label className="form-label" htmlFor="email">
-                Email
-              </label>
+						<div className="form-field mb-6">
+							<label className="form-label" htmlFor="email">
+								Email
+							</label>
 
-              <Input
-                className="w-full h-12 mt-2 hover:border-green-500 focus:border-green-500 active:border-green-600"
-                placeholder="Email Address"
-                value={email}
-                type="email"
-                required={true}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
+							<Input
+								className="w-full h-12 mt-2 hover:border-green-500 focus:border-green-500 active:border-green-600"
+								placeholder="Email Address"
+								value={email}
+								type="email"
+								required={true}
+								onChange={(e) => setEmail(e.target.value)}
+							/>
+						</div>
 
-            <button
-              className={`w-full p-3 mt-6 text-medium rounded-lg mb-8 ${
-                loginValid ? "bg-[#00ff6a] hover:bg-[#58FF9E]" : "bg-[#f5f5f5]"
-              } `}
-              type="submit"
-              onClick={loginValid ? handleSubmit : () => {}}
-            >
-              {loading ? (
-                <BeatLoader color={"#000000"} loading={loading} size={15} />
-              ) : (
-                "Join Waitlist"
-              )}
-            </button>
-          </form>
-        </Modal>
-      )}
-      <div className="Patterns-dots Landing-dots Patterns_animatedIn__2wrQM">
-            <div>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-            <div>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-            <div>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-            <div>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-            <div>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-    </div>
-    
-    // </Layout>
-  );
+						<button
+							className={`w-full p-3 mt-6 text-medium rounded-lg mb-8 ${
+								loginValid ? "bg-[#00ff6a] hover:bg-[#58FF9E]" : "bg-[#f5f5f5]"
+							} `}
+							type="submit"
+							onClick={loginValid ? handleSubmit : () => {}}>
+							{loading ? (
+								<BeatLoader color={"#000000"} loading={loading} size={15} />
+							) : (
+								"Join Waitlist"
+							)}
+						</button>
+					</form>
+				</Modal>
+			)}
+			<div className="Patterns-dots Landing-dots Patterns_animatedIn__2wrQM">
+				<div>
+					<span></span>
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+				<div>
+					<span></span>
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+				<div>
+					<span></span>
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+				<div>
+					<span></span>
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+				<div>
+					<span></span>
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+			</div>
+		</div>
+
+		// </Layout>
+	);
 };
 
 export default Waitlist;
--- src/pages/user-pages/signup.tsx
+++ src/pages/user-pages/signup.tsx
@@ -1,134 +1,154 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FraserButton } from "../../components/Button";
 import Layout from "../../components/layouts/SignInLayout";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useForm, SubmitHandler } from "react-hook-form";
+import { useAppDispatch, useAppSelector } from "../../state/hooks";
+import { registerUserAction } from "../../state/action/user.action";
+import { RootState } from "../../state/redux-store";
+import { _paths_ } from "../../utils/routes";
 
 type Inputs = {
-  firstName: string;
-  lastName: string;
-  email: string;
-  phone: number;
+	first_name: string;
+	last_name: string;
+	email: string;
+	phone: number;
 };
 
 const SignUp = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<Inputs>();
+	const dispatch = useAppDispatch();
+	const { loading, error, userInfo } = useAppSelector(
+		(state: RootState) => state.userLogin
+	);
+	const navigate = useNavigate();
 
-  const onSubmit: SubmitHandler<Inputs> = (data) => {
-    // console.log(data, "formdata");
-  };
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<Inputs>();
 
-  return (
-    <Layout
-      title="Sign In - Fraser"
-      bg={
-        "md:h-screen bg-[#F4F4F4] xs:bg-signup-hero-bg-mobile bg-signin-hero-bg bg-no-repeat"
-      }
-      childClass={"flex justify-center items-center w-full h-5/6 m-auto"}
-    >
-      <div className="w-11/12 sm:w-3/5 lg:w-2/5 mt-4 md:mt-8">
-        <div className="bg-white py-12 px-8 rounded-md w-full">
-          <h1 className="text-2xl lg:text-3xl font-semibold leading-6 tracking-tighter">
-            Signup to continue
-          </h1>
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="w-full mt-8 space-y-5"
-          >
-            <div className="flex md:flex-row flex-col space-y-5 md:space-y-0 w-full">
-              <div className="flex flex-col md:mr-2">
-                <label
-                  htmlFor="firstName"
-                  className="text-[#949292] text-sm md:text-base font-normal"
-                >
-                  First Name
-                </label>
-                <input
-                  {...register("firstName", { required: true })}
-                  autoFocus
-                  className="px-3 py-3 border border-[#BDBDBD] rounded w-full"
-                  type={"text"}
-                  placeholder="John"
-                />
-                {errors.firstName && (
-                  <span className="text-red-500 text-sm">
-                    *First name is required
-                  </span>
-                )}
-              </div>
-              <div className="flex flex-col">
-                <label
-                  htmlFor="lastName"
-                  className="text-[#949292] text-sm md:text-base  font-normal"
-                >
-                  Last Name
-                </label>
-                <input
-                  {...register("lastName", { required: true })}
-                  className="px-3 py-3 border border-[#BDBDBD] rounded w-full"
-                  type={"text"}
-                  placeholder="Doe"
-                />
-                {errors.lastName && (
-                  <span className="text-red-500 text-sm">
-                    *Last name is required
-                  </span>
-                )}
-              </div>
-            </div>
-            <div className="flex flex-col w-full">
-              <label
-                htmlFor="email"
-                className="text-[#949292] text-sm md:text-base font-normal"
-              >
-                Email Address
-              </label>
-              <input
-                {...register("email", { required: true })}
-                className="px-3 py-3 border border-[#BDBDBD] rounded"
-                type={"email"}
-                placeholder="your@email.com"
-              />
-              {errors.email && (
-                <span className="text-red-500 text-sm">*Email is required</span>
-              )}
-            </div>
-            <div className="flex flex-col w-full">
-              <label
-                htmlFor="phoneNumber"
-                className="text-[#949292] text-sm md:text-base font-normal"
-              >
-                Phone Number
-              </label>
-              <input
-                {...register("phone", { required: true })}
-                className="px-3 py-3 border border-[#BDBDBD] rounded"
-                type={"tel"}
-                placeholder="+2348012345678"
-              />
-              {errors.phone && (
-                <span className="text-red-500 text-sm">
-                  *Phone number is required
-                </span>
-              )}
-            </div>
+	const onSubmit: SubmitHandler<Inputs> = async (data) => {
+		dispatch(registerUserAction(data));
+	};
 
-            <FraserButton title="Proceed" type="submit" size="regular" />
-            <Link to={"/"}>
-              <p className="text-center mt-4 text-xs md:text-sm cursor-pointer">
-                Already have an account?{" "}
-                <span className="text-primary-100">Sign in</span>
-              </p>
-            </Link>
-          </form>
-        </div>
-      </div>
-    </Layout>
-  );
+	useEffect(() => {
+		if (userInfo?._id) {
+			navigate(_paths_.BOOKRIDE);
+		}
+	}, [navigate, userInfo]);
+
+	return (
+		<Layout
+			title="Sign In - Fraser"
+			bg={
+				"md:h-screen bg-[#F4F4F4] xs:bg-signup-hero-bg-mobile bg-signin-hero-bg bg-no-repeat"
+			}
+			childClass={"flex justify-center items-center w-full h-5/6 m-auto"}>
+			<div className="w-11/12 sm:w-3/5 lg:w-2/5 mt-4 md:mt-8">
+				<div className="bg-white py-12 px-8 rounded-md w-full">
+					<h1 className="text-2xl lg:text-3xl font-semibold leading-6 tracking-tighter">
+						Signup to continue
+					</h1>
+					{error && <p className="text-red-600">{error}</p>}
+					<form
+						onSubmit={handleSubmit(onSubmit)}
+						className="w-full mt-8 space-y-5">
+						<div className="flex md:flex-row flex-col space-y-5 md:space-y-0 w-full">
+							<div className="flex flex-col md:mr-2">
+								<label
+									htmlFor="firstName"
+									className="text-[#949292] text-sm md:text-base font-normal">
+									First Name
+								</label>
+								<input
+									{...register("first_name", { required: true })}
+									autoFocus
+									className="px-3 py-3 border border-[#BDBDBD] rounded w-full"
+									type={"text"}
+									placeholder="John"
+								/>
+								{errors.first_name && (
+									<span className="text-red-500 text-sm">
+										*First name is required
+									</span>
+								)}
+							</div>
+							<div className="flex flex-col">
+								<label
+									htmlFor="lastName"
+									className="text-[#949292] text-sm md:text-base  font-normal">
+									Last Name
+								</label>
+								<input
+									{...register("last_name", { required: true })}
+									className="px-3 py-3 border border-[#BDBDBD] rounded w-full"
+									type={"text"}
+									placeholder="Doe"
+								/>
+								{errors.last_name && (
+									<span className="text-red-500 text-sm">
+										*Last name is required
+									</span>
+								)}
+							</div>
+						</div>
+						<div className="flex flex-col w-full">
+							<label
+								htmlFor="email"
+								className="text-[#949292] text-sm md:text-base font-normal">
+								Email Address
+							</label>
+							<input
+								{...register("email", { required: true })}
+								className="px-3 py-3 border border-[#BDBDBD] rounded"
+								type={"email"}
+								placeholder="your@email.com"
+							/>
+							{errors.email && (
+								<span className="text-red-500 text-sm">*Email is required</span>
+							)}
+						</div>
+						<div className="flex flex-col w-full">
+							<label
+								htmlFor="phoneNumber"
+								className="text-[#949292] text-sm md:text-base font-normal">
+								Phone Number
+							</label>
+							<input
+								{...register("phone", { required: true })}
+								className="px-3 py-3 border border-[#BDBDBD] rounded"
+								type={"tel"}
+								placeholder="+2348012345678"
+							/>
+							{errors.phone && (
+								<span className="text-red-500 text-sm">
+									*Phone number is required
+								</span>
+							)}
+						</div>
+
+						<FraserButton
+							title="Proceed"
+							loader={loading}
+							type="submit"
+							size="regular"
+						/>
+						{/* <Link to={_paths_.SIGNIN}> */}
+						<p className="text-center mt-4 text-xs md:text-sm cursor-pointer">
+							Already have an account?{" "}
+							<span
+								className="text-primary-100"
+								onClick={() => navigate(_paths_.SIGNIN)}>
+								Sign in
+							</span>
+						</p>
+						{/* </Link> */}
+					</form>
+				</div>
+			</div>
+		</Layout>
+	);
 };
 
 export default SignUp;
