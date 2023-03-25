8f4af9c67d1b3d45baf85fd9907991a38f437ec3
amenolabode <olabodeamen4@gmail.com>
2023-03-25 08:18:42 +0000

    testing vercel

--- src/pages/Waitlist.tsx
+++ src/pages/Waitlist.tsx
@@ -99,7 +99,7 @@ const Waitlist = () => {
 							onClick={() => {
 								navigate(_paths_.BOOKRIDE);
 							}}>
-							Get Early Access
+							Get Early Access 👀
 						</button>
 					</div>
 
